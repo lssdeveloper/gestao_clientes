ed1b770c8e2cc9bc5f73dc02907ec487e594700e
Leandro Serra <leandro.serra.10@gmail.com>
2018-05-29 21:44:31 -0300

    first commit

---
+++ gestao_clientes/clientes/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Documento(models.Model):
+    num_doc = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.num_doc
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    salary = models.DecimalField(max_digits=15, decimal_places=2)
+    bio = models.TextField()
+    photo = models.ImageField(upload_to='clients_photos', null=True, blank=True)
+    # CASCADE O Django deleta os dois objetos Person x Documento
+    doc = models.OneToOneField(Documento, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + '-' + self.last_name
+
+#ManyToMany = Muitos para Muitos
+class Produto(models.Model):
+    descricao = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Venda(models.Model):
+    numero = models.CharField(max_length=7)
+    valor = models.DecimalField(max_digits=15, decimal_places=2)
+    desconto = models.DecimalField(max_digits=15, decimal_places=2)
+    impostos = models.DecimalField(max_digits=15, decimal_places=2)
+    # sE JÁ TIVER VENDA PROTEGE PARA NÃO SER EXCLUÍDO
+    pessoa = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
+    produtos = models.ManyToManyField(Produto, blank=True)
+
+    def __str__(self):
+        return self.numero
